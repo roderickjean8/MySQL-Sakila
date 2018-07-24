6671f377d08650cfdfef646c23fc73a154b11a75
Roderick Jean <roderickjean8@gmail.com>
2018-07-24 16:08:55 -0400

    adding 4a to 6c solution

--- Sakila_db_HW.sql
+++ Sakila_db_HW.sql
@@ -26,3 +26,28 @@ ORDER BY last_name, first_name;
 SELECT country_id, country 
 FROM country
 WHERE country IN('Afghanistan','Bangladesh', 'China') 
+
+--3a
+ALTER TABLE actor 
+ADD COLUMN
+middle_name varchar(40) AFTER first_name;
+
+--3b
+SELECT * FROM actor;
+
+ALTER TABLE actor
+MODIFY middle_name BLOB NOT NULL;
+
+--3c
+ALTER TABLE actor 
+DROP middle_name;
+
+SELECT * FROM actor;
+
+--4a
+SELECT last_name, 
+COUNT(*) AS COUNT
+FROM actor
+HAVING COUNT >=2;
+
+--4b
