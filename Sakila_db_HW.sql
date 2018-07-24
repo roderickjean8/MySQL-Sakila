e63e95311733034420bd5abcd0368a7d180568ea
Roderick Jean <roderickjean8@gmail.com>
2018-07-23 20:02:12 -0400

    adding 2a 2b 2c 2d solution

--- Sakila_db_HW.sql
+++ Sakila_db_HW.sql
@@ -6,3 +6,23 @@ FROM actor;
 SELECT UPPER(CONCAT(first_name,' ', last_name)) AS 'Actor Name'
 FROM actor;
 
+--2a.
+SELECT first_name, last_name, actor_id 
+FROM actor
+where first_name = 'Joe';
+
+--2b.
+SELECT first_name, last_name, actor_id 
+FROM actor
+WHERE last_name LIKE '%Gen%';
+
+--2c 
+SELECT first_name, last_name, actor_id 
+FROM actor
+WHERE last_name LIKE '%Li%' 
+ORDER BY last_name, first_name;
+
+--2d
+SELECT country_id, country 
+FROM country
+WHERE country IN('Afghanistan','Bangladesh', 'China') 
